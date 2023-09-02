cbffba66935eda34a34827eb75dbe51635d25158
Daineka A.U <zanoza193@mail.ru>
2023-09-02 22:06:42 +0300

    Initial commit

---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+
+public class PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Post> all() {
+    return repository.all();
+  }
+
+  public Post getById(long id) throws Exception {
+    if (id == 0) throw new Exception("Такой \"id\" не существует");
+    return repository.getById(id);
+  }
+
+  public Post save(Post post) throws Exception {
+    return repository.save(post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
+
